283c0bb1cf01160ff344ea1b630844e16375e52c
Personfrommoon <106992165+Personfrommoon@users.noreply.github.com>
2024-09-18 21:50:07 +0300

    Create Backup.ps1

---
+++ Backup.ps1
@@ -0,0 +1,18 @@
+$ServerInstance = "backups"
+$DatabaseName = "rimi_kristofervalk.sql"
+$BackupDir = "C:\users\backups\databases"
+$BackupFile = "${BackupDir}\${DatabaseName}"
+
+$T = @{
+  Frequency="Daily"
+  At="4:00AM"
+}
+$O = @{
+  WakeToRun=$true
+  StartIfNotIdle=$false
+  MultipleInstancePolicy="Queue"
+}
+Register-ScheduledJob -Trigger $T -ScheduledJobOption $O -scriptblock {
+    mysqldump -u root -pPassw0rd rimi-database > $DatabaseName
+    pscp -pw PASSWORD $DatabaseName USERNAME@BACKUPSERVER:$BackupFile
+}
